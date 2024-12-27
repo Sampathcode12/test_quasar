80665b2618f6e0732809bc8960815219a1dfb4ce
Sampathcode12 <lsampathlk4@gmail.com>
2024-12-27 22:58:47 +0530

    init2

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,19 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'about', component: () => import('src/pages/AboutPage.vue') },
+      { path: 'contuct', component: () => import('src/pages/contuctPage.vue') },
+      { path: 'index', component: () => import('src/pages/IndexPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
